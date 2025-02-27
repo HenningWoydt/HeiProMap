c99bdd400824e47652033f31b754cb9268e7235a
henning <henning.woydt@gmail.com>
2025-02-27 14:28:45 +0100

    * improved own label propagation

--- src/serial/refinement/label_propagation_refinement.h
+++ src/serial/refinement/label_propagation_refinement.h
@@ -44,34 +44,41 @@ namespace HeiProMap {
     };
 
     class LabelPropagationRefinement final : public ISerialRefiner {
-        vertex_t m_n    = 0;
-        vertex_t m_m    = 0;
-        partition_t m_k = 0;
-        weight_t m_lmax = 0;
+        vertex_t                 m_n    = 0;
+        vertex_t                 m_m    = 0;
+        partition_t              m_k    = 0;
+        weight_t                 m_lmax = 0;
         std::vector<partition_t> m_hierarchy;
-        std::vector<weight_t> m_distance;
-        u64 m_seed = 0;
+        std::vector<weight_t>    m_distance;
+        u64                      m_seed = 0;
 
-        std::vector<u32> block_used;
-        u32 block_marker = 0;
-
-        std::vector<vertex_t> vertices_this_round;
-        std::vector<vertex_t> vertices_next_round;
-        std::vector<u32> vertex_added;
+        u32 *vertex_used  = nullptr;
         u32 vertex_marker = 0;
 
-        std::mt19937 gen;
+        u32 *block_used  = nullptr;
+        u32 block_marker = 0;
+
+        vertex_t *curr_boundary     = nullptr;
+        size_t   curr_boundary_size = 0;
+
+        std::mt19937                          gen;
         std::uniform_real_distribution<float> dis;
 
     public:
         LabelPropagationRefinement() = default;
 
+        ~LabelPropagationRefinement() {
+            free(vertex_used);
+            free(block_used);
+            free(curr_boundary);
+        }
+
         void initialize(const vertex_t t_n,
                         const vertex_t t_m,
                         const partition_t t_k,
                         const weight_t t_lmax,
-                        const std::vector<partition_t>& t_hierarchy,
-                        const std::vector<weight_t>& t_distance,
+                        const std::vector<partition_t> &t_hierarchy,
+                        const std::vector<weight_t> &t_distance,
                         const u64 t_seed) override {
             m_n         = t_n;
             m_m         = t_m;
@@ -81,21 +88,29 @@ namespace HeiProMap {
             m_distance  = t_distance;
             m_seed      = t_seed;
 
-            block_used.resize(t_k, block_marker);
-            vertex_added.resize(t_n, vertex_marker);
+            vertex_t m_n_64 = round_up_64(m_n);
+            vertex_used = (u32 *) aligned_alloc(64, m_n_64 * sizeof(u32));
+            std::fill_n(vertex_used, m_n_64, vertex_marker);
+
+            partition_t m_k_64 = round_up_64(m_k);
+            block_used = (u32 *) aligned_alloc(64, m_k_64 * sizeof(u32));
+            std::fill_n(block_used, m_k_64, block_marker);
+
+            curr_boundary      = (vertex_t *) aligned_alloc(64, m_n_64 * sizeof(vertex_t));
+            curr_boundary_size = 0;
 
             gen.seed(m_seed);
             dis = std::uniform_real_distribution<float>(0.0f, 1.0f);
         }
 
-        template <typename TSerialGraph, typename TSerialActiveVertexManager, typename TSerialBoundaryVertexManager, typename TSerialPartitionManager, typename TSerialDistanceOracle, typename TSerialQuotientGraph>
-        void refine(LabelPropagationConfiguration& config,
-                    TSerialGraph& g,
-                    [[maybe_unused]] TSerialActiveVertexManager& av_manager,
-                    TSerialBoundaryVertexManager& bv_manager,
-                    TSerialPartitionManager& p_manager,
-                    TSerialDistanceOracle& d_oracle,
-                    TSerialQuotientGraph& q_graph) {
+        template<typename TSerialGraph, typename TSerialActiveVertexManager, typename TSerialBoundaryVertexManager, typename TSerialPartitionManager, typename TSerialDistanceOracle, typename TSerialQuotientGraph>
+        void refine(LabelPropagationConfiguration &config,
+                    TSerialGraph &g,
+                    [[maybe_unused]] TSerialActiveVertexManager &av_manager,
+                    TSerialBoundaryVertexManager &bv_manager,
+                    TSerialPartitionManager &p_manager,
+                    TSerialDistanceOracle &d_oracle,
+                    TSerialQuotientGraph &q_graph) {
             static_assert(std::is_base_of_v<ISerialGraph, TSerialGraph>, "TSerialGraph must inherit from ISerialGraph");
             static_assert(std::is_base_of_v<ISerialActiveVertexManager, TSerialActiveVertexManager>, "TSerialActiveVertexManager must inherit from ISerialActiveVertexManager");
             static_assert(std::is_base_of_v<ISerialBoundaryVertexManager, TSerialBoundaryVertexManager>, "TSerialBoundaryVertexManager must inherit from ISerialBoundaryVertexManager");
@@ -103,95 +118,70 @@ namespace HeiProMap {
             static_assert(std::is_base_of_v<ISerialDistanceOracle, TSerialDistanceOracle>, "TSerialDistanceOracle must inherit from ISerialDistanceOracle");
             static_assert(std::is_base_of_v<ISerialQuotientGraph, TSerialQuotientGraph>, "TSerialQuotientGraph must inherit from ISerialQuotientGraph");
 
-            vertices_this_round.clear();
-            for (vertex_t u : bv_manager) { vertices_this_round.push_back(u); }
+            bool     move_occurred = true;
+            for (u64 iteration     = 0; iteration < config.max_iteration && move_occurred; ++iteration) {
+                move_occurred = false;
 
-            for (u64 iteration = 0; iteration < config.max_iteration && !vertices_this_round.empty(); ++iteration) {
+                curr_boundary_size = 0;
+                for (vertex_t u: bv_manager) { curr_boundary[curr_boundary_size++] = u; }
+                std::shuffle(curr_boundary, curr_boundary + curr_boundary_size, gen);
+
+                // std::sort(curr_boundary, curr_boundary + curr_boundary_size, [&](vertex_t u, vertex_t v) { return g.size(u) > g.size(v); });
+                // std::sort(curr_boundary, curr_boundary + curr_boundary_size, [&](vertex_t u, vertex_t v) { return g.get_weight(u) > g.get_weight(v); });
+
                 vertex_marker += 1;
+                for (size_t i = 0; i < curr_boundary_size; ++i) {
+                    vertex_t u = curr_boundary[i];
+                    if (vertex_used[u] == vertex_marker) { continue; }
+                    if (!bv_manager.is_boundary(u)) { continue; }
 
-                for (vertex_t u : vertices_this_round) {
-                    if (!is_boundary(g, p_manager, u)) { continue; }
-
-                    weight_t u_weight = g.get_weight(u);
-                    partition_t u_id  = p_manager[u];
+                    weight_t    u_weight = g.get_weight(u);
+                    partition_t u_id     = p_manager[u];
 
                     // make the move that reduces qap the most
-                    partition_t best_u_id = u_id;
-                    s64 best_qap_delta    = -1;
-                    u32 counter           = 0;
-                    bool all_overloaded   = true;
+                    partition_t best_id        = u_id;
+                    weight_t    best_id_weight = 0;
+                    s64         best_qap_delta = -1;
+                    f32         counter        = 0;
 
                     block_marker += 1;
-                    for (const auto& [v, w] : g[u]) {
-                        partition_t v_id = p_manager[v];
-                        if (v_id == u_id || v_id == best_u_id) { continue; }
+                    block_used[u_id] = block_marker;
+                    for (const auto [v, w]: g[u]) {
+                        partition_t v_id        = p_manager[v];
+                        weight_t    v_id_weight = p_manager.get_bweight(v_id);
 
                         if (block_used[v_id] != block_marker) {
-                            if (p_manager.get_bweight(v_id) + u_weight <= m_lmax) {
-                                all_overloaded = false;
-                                s64 qap_delta  = get_u_qap_delta(g, u, u_id, v_id, p_manager, d_oracle);
-                                if (qap_delta < 0) {
-                                    block_used[v_id] = block_marker;
-                                    continue;
-                                }
+                            if (v_id_weight + u_weight <= m_lmax) {
+                                s64 qap_delta = get_u_qap_delta(g, u, u_id, v_id, p_manager, d_oracle);
 
-                                if (qap_delta > best_qap_delta) {
-                                    // straight better so chose this
+                                if (qap_delta > best_qap_delta || (qap_delta == best_qap_delta && v_id_weight < best_id_weight)) {
+                                    best_id        = v_id;
+                                    best_id_weight = v_id_weight;
                                     best_qap_delta = qap_delta;
-                                    best_u_id      = v_id;
-                                    counter        = 1;
-                                } else if (qap_delta == best_qap_delta) {
-                                    if (p_manager.get_bweight(v_id) < p_manager.get_bweight(best_u_id)) {
-                                        // same, but another partition has smaller weight
-                                        best_u_id = v_id;
-                                        counter   = 1;
-                                    } else {
-                                        // same delta and same weight, so uniform choosing
-                                        counter += 1;
-                                        if (dis(gen) < 1.0f / (f32)counter) {
-                                            best_u_id = v_id;
-                                        }
+                                    counter        = 1.0;
+                                } else if (qap_delta == best_qap_delta && qap_delta != -1) {
+                                    counter += 1.0;
+                                    // choose with probability 1/counter as it ensures uniform distribution
+                                    if (dis(gen) < 1.0f / counter) {
+                                        best_id = v_id;
                                     }
                                 }
-                                block_used[v_id] = block_marker;
                             }
+                            block_used[v_id] = block_marker;
                         }
                     }
 
-                    if (best_u_id != u_id) {
+                    if (best_id != u_id) {
                         // choose if positive, if 0-gain choose 50% of the time
                         if (best_qap_delta > 0 || dis(gen) < 0.5) {
-                            bv_manager.move(g, p_manager, u, u_id, best_u_id);
-                            q_graph.move(g, p_manager, u, u_id, best_u_id);
-                            p_manager.move(u, u_weight, u_id, best_u_id);
-
-                            // u gets checked again
-                            if (vertex_added[u] != vertex_marker) {
-                                vertices_next_round.push_back(u);
-                                vertex_added[u] = vertex_marker;
-                            }
-
-                            // neighborhood of u also gets checked
-                            for (const auto& [v, w] : g[u]) {
-                                if (vertex_added[v] != vertex_marker) {
-                                    vertices_next_round.push_back(v);
-                                    vertex_added[v] = vertex_marker;
-                                }
-                            }
+                            bv_manager.move(g, p_manager, u, u_id, best_id);
+                            q_graph.move(g, p_manager, u, u_id, best_id);
+                            p_manager.move(u, u_weight, u_id, best_id);
+                            move_occurred = true;
                         }
                     }
-
-                    if (all_overloaded) {
-                        // u gets checked again
-                        if (vertex_added[u] != vertex_marker) {
-                            vertices_next_round.push_back(u);
-                            vertex_added[u] = vertex_marker;
-                        }
-                    }
+                    vertex_used[u] = vertex_marker;
                 }
-                // swap in vertices of next round
-                std::swap(vertices_this_round, vertices_next_round);
-                vertices_next_round.clear();
             }
         }
     };
