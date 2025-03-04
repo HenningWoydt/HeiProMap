f3bae3035746a93bb15ea54639629168ce35caa0
henning <henning.woydt@gmail.com>
2025-03-04 14:19:35 +0100

    * changed the threshold for initial partitioning

--- src/serial/datastructures/solver.h
+++ src/serial/datastructures/solver.h
@@ -170,7 +170,7 @@ namespace HeiProMap {
         void internal_solve() {
             s32 level = 0;
 
-            while (av_manager.get_n_active() > ac.k * 16) {
+            while (av_manager.get_n_active() > ac.k * 32) {
                 matching(level);
                 coarsening(level);
 
