b11d11fd32ed76d70c267abe30308b36ff9a0915
henning <henning.woydt@gmail.com>
2025-03-05 19:58:07 +0100

    * decreased threshold

--- src/serial/datastructures/solver.h
+++ src/serial/datastructures/solver.h
@@ -170,7 +170,7 @@ namespace HeiProMap {
         void internal_solve() {
             s32 level = 0;
 
-            while (av_manager.get_n_active() > ac.k * 64) {
+            while (av_manager.get_n_active() > ac.k * 4) {
                 matching(level);
                 coarsening(level);
 
