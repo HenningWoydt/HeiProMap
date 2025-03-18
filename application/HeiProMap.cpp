d7afe1b8984cf133247db5123f540486b00eacc4
henning <henning.woydt@gmail.com>
2025-03-18 10:15:13 +0100

    * update fastest config

--- application/HeiProMap.cpp
+++ application/HeiProMap.cpp
@@ -62,7 +62,7 @@ int main(const int argc, char* argv[]) {
                     {"--hierarchy", "4:8:6"},
                     {"--distance", "1:10:100"},
                     {"--imbalance", "0.03"},
-                    {"--config", "fast"},
+                    {"--config", "fastest"},
                     {"--seed", "0"},
 
                     // coarsening
