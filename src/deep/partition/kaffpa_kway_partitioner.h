d2dd48af7d8854b1d7b2cddfe0cd16553c874413
henning <henning.woydt@gmail.com>
2025-09-08 09:33:37 +0200

    * simplified partitioning

--- src/deep/partition/kaffpa_kway_partitioner.h
+++ src/deep/partition/kaffpa_kway_partitioner.h
@@ -27,16 +27,11 @@
 #ifndef HEIPROMAP_KAFFPA_K_WAY_PARTITIONER_H
 #define HEIPROMAP_KAFFPA_K_WAY_PARTITIONER_H
 
-#include <atomic>
 #include <string>
 
 #include "kaHIP_interface.h"
 
-#include "../../serial/serial_definitions_1.h"
-#include "../../serial/serial_definitions_2.h"
-#include "../../serial/serial_definitions_3.h"
 #include "../../commons/random_engine.h"
-#include "../../commons/statistic_collector.h"
 #include "../../src/commons/small_translation_table.h"
 
 namespace HeiProMap {
@@ -88,6 +83,10 @@ namespace HeiProMap {
             int *adjcwgt = nullptr;
             int *adjncy = nullptr;
             int *part = nullptr;
+            int *best_part = nullptr;
+
+            int edge_cut = 0;
+            int best_edge_cut = 0;
 
             size_t n_vertices = 0;
             size_t n_edges = 0;
@@ -114,6 +113,8 @@ namespace HeiProMap {
 
             m_kaffpa_vars.resize(m_threads);
 
+            blocks.resize(m_k);
+
             random_engine = &t_random_engine;
             config = dynamic_cast<const KaffpaKWayPartitionerConfiguration *>(&t_config);
         }
@@ -125,32 +126,106 @@ namespace HeiProMap {
                 free(var.adjcwgt);
                 free(var.adjncy);
                 free(var.part);
+                free(var.best_part);
             }
         }
 
-        /**
-         * Partitions the subgraph of g where the vertices have the corresponding id.
-         *
-         * @param g The graph to partition.
-         * @param p_manager The partition manager.
-         * @param id The id of the subgraph to partition.
-         * @param k The number of partitions.
-         * @param lmax The maximum weight of a partition.
-         */
-        void partition(const deep_graph_t &g,
-                       deep_p_manager_t &p_manager,
-                       u64 thread_id,
-                       partition_t id,
-                       partition_t id_increment,
-                       partition_t k,
-                       weight_t lmax,
-                       partition_t hierarchy_level) {
+        void initial_partition(const deep_graph_t &g,
+                               deep_p_manager_t &p_manager,
+                               partition_t k,
+                               partition_t k_spacing,
+                               weight_t lmax,
+                               partition_t hierarchy_level,
+                               size_t kappa = 10) {
 
-            if (blocks[id].empty()) {
-                for (partition_t i = 0; i < k; ++i) {
-                    partition_t move_id = id + id_increment * i;
-                    p_manager.set_lmax(move_id, lmax);
-                    p_manager.set_hierarchy_level(move_id, hierarchy_level - 1);
+#pragma omp parallel for num_threads(m_threads) schedule(static)
+            for (u64 thread_id = 0; thread_id < m_threads; thread_id++) {
+                KaffpaVars &var = m_kaffpa_vars[thread_id];
+
+                // step 1: extract the number of vertices and edges
+                vertex_t subgraph_n_vertices = g.get_n();
+                vertex_t subgraph_n_edges = g.get_m();
+                weight_t subgraph_weight = g.weight();
+
+                allocate(subgraph_n_vertices, subgraph_n_edges, thread_id);
+                var.xadj[0] = 0;
+                // step 2: build graph in kaffpa variables
+                forall_gu(g, u)
+                    {
+                        var.vwgt[u] = (int) g.weight(u);
+                        var.xadj[u + 1] = var.xadj[u];
+                        forall_guivw(g, u, i, v, w) {
+                                var.adjncy[var.xadj[u + 1]] = (int) v;
+                                var.adjcwgt[var.xadj[u + 1]] = (int) w;
+                                var.xadj[u + 1] += 1;
+                            }
+                        endfor
+                    }
+                endfor
+
+                // step 3: calculate allowed imbalance
+                f64 imbalance = ((f64) (lmax * k) / (f64) subgraph_weight) - 1.0;
+                if (imbalance <= 0.0) {
+                    // Failsafe if subgraph weight is too large // TODO: would be cool if not needed
+                    imbalance = 0.0001;
+                }
+
+                // step 4: partition
+                int n = (int) subgraph_n_vertices;
+                int n_parts = (int) k;
+
+                var.best_edge_cut = std::numeric_limits<int>::max();
+
+                for (size_t i = 0; i < std::max((size_t) 1, kappa); ++i) {
+                    kaffpa(&n, var.vwgt, var.xadj, var.adjcwgt, var.adjncy, &n_parts, &imbalance, true, 0, FAST, &var.edge_cut, var.part);
+                    if (var.edge_cut < var.best_edge_cut) {
+                        std::swap(var.edge_cut, var.best_edge_cut);
+                        std::swap(var.part, var.best_part);
+                    }
+                }
+            }
+
+            // choose the best across all partitioning
+            for (u64 thread_id = 1; thread_id < m_threads; thread_id++) {
+                if (m_kaffpa_vars[thread_id].best_edge_cut < m_kaffpa_vars[0].best_edge_cut) {
+                    std::swap(m_kaffpa_vars[0].best_edge_cut, m_kaffpa_vars[thread_id].best_edge_cut);
+                    std::swap(m_kaffpa_vars[0].best_part, m_kaffpa_vars[thread_id].best_part);
+                }
+            }
+
+            // step 5: extract partition into p_manager
+            forall_gu(g, u)
+                {
+                    if (m_kaffpa_vars[0].best_part[u] == 0) { continue; }
+
+                    partition_t old_id = 0;
+                    partition_t new_id = m_kaffpa_vars[0].best_part[u] * k_spacing;
+
+                    p_manager.move(u, g.weight(u), old_id, new_id);
+                }
+            endfor
+
+            for (partition_t i = 0; i < k; ++i) {
+                partition_t move_id = k_spacing * i;
+                p_manager.set_lmax(move_id, lmax);
+                p_manager.set_hierarchy_level(move_id, hierarchy_level - 1);
+            }
+        }
+
+        void intermediate_partition(const deep_graph_t &g,
+                                    deep_p_manager_t &p_manager,
+                                    u64 thread_id,
+                                    partition_t block_id,
+                                    partition_t k_spacing,
+                                    partition_t block_k,
+                                    weight_t block_lmax,
+                                    partition_t block_level,
+                                    size_t kappa = 1) {
+            if (blocks[block_id].empty()) {
+                for (partition_t i = 0; i < block_k; ++i) {
+                    partition_t move_id = block_id + k_spacing * i;
+                    p_manager.set_lmax(move_id, block_lmax);
+                    p_manager.set_hierarchy_level(move_id, block_level - 1);
                 }
             }
 
@@ -162,12 +237,12 @@ namespace HeiProMap {
             vertex_t subgraph_n_edges = 0;
             weight_t subgraph_weight = 0;
             // step 1: extract the number of vertices and edges, build tt
-            for (vertex_t u: blocks[id]) {
+            for (vertex_t u: blocks[block_id]) {
                 var.tt.add(u, subgraph_n_vertices);
                 subgraph_n_vertices += 1;
                 subgraph_weight += g.weight(u);
                 forall_guiv(g, u, i, v) {
-                        if (p_manager[v] == id) { subgraph_n_edges += 1; }
+                        if (p_manager[v] == block_id) { subgraph_n_edges += 1; }
                     }
                 endfor
             }
@@ -175,12 +250,12 @@ namespace HeiProMap {
             allocate(subgraph_n_vertices, subgraph_n_edges, thread_id);
             var.xadj[0] = 0;
             // step 2: build graph in kaffpa variables
-            for (vertex_t u: blocks[id]) {
+            for (vertex_t u: blocks[block_id]) {
                 vertex_t new_u = var.tt.get_n(u);
                 var.vwgt[new_u] = (int) g.weight(u);
                 var.xadj[new_u + 1] = var.xadj[new_u];
                 forall_guivw(g, u, i, v, w) {
-                        if (p_manager[v] == id) {
+                        if (p_manager[v] == block_id) {
                             vertex_t new_v = var.tt.get_n(v);
                             var.adjncy[var.xadj[new_u + 1]] = (int) new_v;
                             var.adjcwgt[var.xadj[new_u + 1]] = (int) w;
@@ -191,40 +266,41 @@ namespace HeiProMap {
             }
 
             // step 3: calculate allowed imbalance
-            f64 imbalance = ((f64) (lmax * k) / (f64) subgraph_weight) - 1.0;
+            f64 imbalance = ((f64) (block_lmax * block_k) / (f64) subgraph_weight) - 1.0;
             if (imbalance <= 0.0) {
                 // Failsafe if subgraph weight is too large // TODO: would be cool if not needed
-                // std::cout << "failsafe " << imbalance << " " << lmax << " " << k << " " << subgraph_weight << std::endl;
                 imbalance = 0.0001;
             }
 
             // step 4: partition
             int n = (int) subgraph_n_vertices;
-            int n_parts = (int) k;
-            int edge_cut;
+            int n_parts = (int) block_k;
 
-            kaffpa(&n, var.vwgt, var.xadj, var.adjcwgt, var.adjncy, &n_parts, &imbalance, true, 0, FAST, &edge_cut, var.part);
+            var.best_edge_cut = std::numeric_limits<int>::max();
+            for (size_t i = 0; i < kappa; ++i) {
+                kaffpa(&n, var.vwgt, var.xadj, var.adjcwgt, var.adjncy, &n_parts, &imbalance, true, 0, FAST, &var.edge_cut, var.part);
+                if (var.edge_cut < var.best_edge_cut) {
+                    std::swap(var.edge_cut, var.best_edge_cut);
+                    std::swap(var.part, var.best_part);
+                }
+            }
 
-            // m_mutex.lock();
             // step 5: extract partition into p_manager
             for (vertex_t kaffpa_vertex = 0; kaffpa_vertex < subgraph_n_vertices; ++kaffpa_vertex) {
-                if (var.part[kaffpa_vertex] == 0) { continue; }
-                partition_t move_id = id + id_increment * var.part[kaffpa_vertex];
+                if (var.best_part[kaffpa_vertex] == 0) { continue; }
+                partition_t move_id = block_id + k_spacing * var.best_part[kaffpa_vertex];
 
-                partition_t vertex_id = id;
+                partition_t vertex_id = block_id;
                 vertex_t vertex = var.tt.get_o(kaffpa_vertex);
                 weight_t vertex_weight = g.weight(vertex);
 
-                // bv_manager.move(g, p_manager, vertex, vertex_id, move_id);
-                // q_graph.move(g, p_manager, vertex, vertex_id, move_id);
                 p_manager.move(vertex, vertex_weight, vertex_id, move_id);
             }
-            // m_mutex.unlock();
 
-            for (partition_t i = 0; i < k; ++i) {
-                partition_t move_id = id + id_increment * i;
-                p_manager.set_lmax(move_id, lmax);
-                p_manager.set_hierarchy_level(move_id, hierarchy_level - 1);
+            for (partition_t i = 0; i < block_k; ++i) {
+                partition_t move_id = block_id + k_spacing * i;
+                p_manager.set_lmax(move_id, block_lmax);
+                p_manager.set_hierarchy_level(move_id, block_level - 1);
             }
         }
 
@@ -239,10 +315,12 @@ namespace HeiProMap {
                 free(var.vwgt);
                 free(var.xadj);
                 free(var.part);
+                free(var.best_part);
 
                 var.vwgt = (int *) malloc(var.n_vertices * sizeof(int));
                 var.xadj = (int *) malloc((var.n_vertices + 1) * sizeof(int));
                 var.part = (int *) malloc(var.n_vertices * sizeof(int));
+                var.best_part = (int *) malloc(var.n_vertices * sizeof(int));
             }
 
             if (new_n_edges > var.n_edges) {
@@ -257,9 +335,7 @@ namespace HeiProMap {
         }
 
         void determine_all_blocks(const deep_graph_t &g,
-                                  deep_p_manager_t &p_manager) {
-            blocks.resize(m_k);
-
+                                  const deep_p_manager_t &p_manager) {
             for (partition_t id = 0; id < m_k; ++id) {
                 blocks[id].clear();
             }
