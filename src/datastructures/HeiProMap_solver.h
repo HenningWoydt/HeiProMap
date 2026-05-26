4c2bf9a68a841995e99ed8f8516853a82acb8ef7
henning <henning.woydt@gmail.com>
2026-05-26 17:08:53 +0200

    * recursive solver uses HeiPa

--- src/datastructures/HeiProMap_solver.h
+++ src/datastructures/HeiProMap_solver.h
@@ -727,8 +727,15 @@ namespace HeiProMap {
                     kaffpa_partition(g, k, per_level_epsilon, KAFFPA_PARTITION_STRONG, ac.seed, partition, ac.global_multisection_config.kappa);
                 } else if (ac.global_multisection_config.mode == GLOBAL_MULTISECTION_KAFFPA_ECO) {
                     kaffpa_partition(g, k, per_level_epsilon, KAFFPA_PARTITION_ECO, ac.seed, partition, ac.global_multisection_config.kappa);
-                } else {
+                } else if (ac.global_multisection_config.mode == GLOBAL_MULTISECTION_KAFFPA_FAST) {
                     kaffpa_partition(g, k, per_level_epsilon, KAFFPA_PARTITION_FAST, ac.seed, partition, ac.global_multisection_config.kappa);
+                } else if (ac.global_multisection_config.mode >= GLOBAL_MULTISECTION_HEIPA_FAST && ac.global_multisection_config.mode <= GLOBAL_MULTISECTION_HEIPA_SUPER_STRONG) {
+                    heipa_multisection_partition_wrapper(g, k, per_level_epsilon, ac.seed, partition, ac.global_multisection_config.mode);
+                } else if (ac.global_multisection_config.mode == GLOBAL_MULTISECTION_METIS_KWAY) {
+                    kway_partition(g, k, per_level_epsilon, ac.seed, partition, ac.global_multisection_config.kappa);
+                } else {
+                    std::cerr << "Mode " << ac.global_multisection_config.mode << " not implemented" << std::endl;
+                    abort();
                 }
             }
 
