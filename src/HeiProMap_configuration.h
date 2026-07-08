59d9b685d629c08b615b953be8240c1da2981499
henning <henning.woydt@gmail.com>
2026-07-08 18:22:40 +0200

    * restore fast config

--- src/HeiProMap_configuration.h
+++ src/HeiProMap_configuration.h
@@ -472,7 +472,7 @@ namespace HeiProMap {
             label_propagation_config.use_parallel_alg = false;
 
             // enable quotient graph refinement
-            quotient_graph_refinement_config.enabled = false;
+            quotient_graph_refinement_config.enabled = true;
             quotient_graph_refinement_config.max_iteration = 2;
             quotient_graph_refinement_config.alpha = 5.0;
             quotient_graph_refinement_config.min_n_steps = 3;
